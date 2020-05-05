3e8869fc5a082181e5f42c9bc7f8239df48366a5
papraczy <patrycja.praczyk@cern.ch>
2020-05-05 10:09:40 +0200

    fixed checksum bug (2)

--- model/Data.py
+++ model/Data.py
@@ -20,9 +20,11 @@ class Data:
         return str(vars(self))
 
     def add_header_info(self, data_header: str):
+        self.complete_data += data_header
         self.extract_packet_length(data_header)
         self.extract_data_counter(data_header)
         self.extract_message_code(data_header)
+        self.extract_checksum(data_header)
 
     def extract_packet_length(self, data_header: str):
         self.packet_len = StrManipulator.substring(data_header, GlobalConstants.PACKET_LEN_START_INDEX,
@@ -42,3 +44,8 @@ class Data:
                                               GlobalConstants.DATA_COUNTER_END_INDEX)
         self.data_index = Calculator.get_int(data_index)
         self.data_index_hex = data_index
+
+    def extract_checksum(self, data_header: str):
+        checksum = StrManipulator.substring(data_header, GlobalConstants.CHECKSUM_START_INDEX,
+                                            GlobalConstants.CHECKSUM_END_INDEX)
+        self.checksum = Calculator.get_int(checksum)
