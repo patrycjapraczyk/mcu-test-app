b852cd50f1be20a7dcdc08c25800cd7df333eca0
papraczy <patrycja.praczyk@cern.ch>
2020-05-21 12:28:47 +0200

    added ecc_check into hardware  simulator

--- test/TestSerialCom.py
+++ test/TestSerialCom.py
@@ -24,31 +24,48 @@ class TestSerialCom:
         self.send_data_queue = Queue()
         self.read_data_queue = Queue()
         self.new_data_to_read = True
-
+        self.ecc_index = 0
 
     def init_com(self):
         """
         opens a serial communication port
         """
         self.serial_port = serial.Serial(self.COM_PORT_NAME, self.BAUDRATE,
-                                        self.BYTESIZE, serial.PARITY_NONE, serial.STOPBITS_ONE, timeout=0)
+                                         self.BYTESIZE, serial.PARITY_NONE, serial.STOPBITS_ONE, timeout=0)
 
     def send_data_packet(self, data):
         """
         send a generated data packet to the connected
-        serial communication port
+        serial communication port, increments data_cnt
         """
+        DataPacketFactory.adjust_data_cnt(data, self.data_cnt)
         self.serial_port.write(data)
         self.data_cnt += 1
 
-    def generate_ecc_check(self):
-        ecc_check_frame = 'aa002000000000168d0499040000ffff00010000000201a40003000000040081'
-        byte_len = int(len(ecc_check_frame)/2)
-        ecc_check_frame = Calculator.get_int(ecc_check_frame)
-        ecc_check_frame = Calculator.get_bytearray(ecc_check_frame, byte_len)
-        while True :
+    def add_ecc_check_to_queue(self, ecc_addresses: list):
+        err_cnt = len(ecc_addresses)
+        overflow = len(ecc_addresses) > GlobalConstants.MAX_ADDRESSES_ECC
+        split_addresses = DataStructFunctions.chunk(ecc_addresses, GlobalConstants.MAX_ADDRESSES_ECC)
+
+        for addr in split_addresses:
+            addr = ''.join(addr)
+            ecc_check_frame = DataPacketFactory.get_packet('ECC_CHECKED',
+                                                           params={
+                                                               'index': self.ecc_index,
+                                                               'overflow': overflow,
+                                                               'err_cnt': err_cnt,
+                                                               'ecc_addresses': addr
+                                                           })
             self.send_data_queue.put(ecc_check_frame)
-            time.sleep(self.cur_ecc_period/1000)
+
+        self.ecc_index += 1
+
+    def generate_ecc_check(self):
+        while True:
+            ecc_addresses = ['000000', '111111', '222222', '333333', '444444', '555555', '666666', '777777', '888888',
+                             '999999']
+            self.add_ecc_check_to_queue(ecc_addresses)
+            time.sleep(self.cur_ecc_period / 1000)
 
     def read_data(self):
         """
@@ -97,17 +114,20 @@ class TestSerialCom:
             self.send_data_stream(heartbeat)
 
     def analyse_heartbeat(self, data_payload: str):
-        period = StrManipulator.substring(data_payload, GlobalConstants.HEARTBEAT_PERIOD_START, GlobalConstants.HEARTBEAT_PERIOD_END)
+        period = StrManipulator.substring(data_payload, GlobalConstants.HEARTBEAT_PERIOD_START,
+                                          GlobalConstants.HEARTBEAT_PERIOD_END)
         period = Calculator.get_int(period)
         self.curr_heartbeat_period = DataStructFunctions.get_key(GlobalConstants.HEARTBEAT_PERIODS, period)
-        id = StrManipulator.substring(data_payload, GlobalConstants.HEARTBEAT_ID_START, GlobalConstants.HEARTBEAT_ID_END)
+        id = StrManipulator.substring(data_payload, GlobalConstants.HEARTBEAT_ID_START,
+                                      GlobalConstants.HEARTBEAT_ID_END)
         id = Calculator.get_int(id)
-        heartbeat = DataPacketFactory.get_heartbeat_response(self.data_cnt, {'heartbeat_id': id})
+        heartbeat = DataPacketFactory.get_packet('HEARTBEAT_RESPONSE', params={'heartbeat_id': id})
         return heartbeat
 
     def send_data_stream(self, heartbeat: bytearray):
         data_sent = 0
         max_frames = SerialManager.get_max_frames_num(self.curr_heartbeat_period, TestSerialCom.BAUDRATE)
+        print('MAX FRAMES: ' + str(max_frames))
         while (not self.send_data_queue.empty()) and data_sent < max_frames:
             data = self.send_data_queue.get()
             self.send_data_packet(data)
